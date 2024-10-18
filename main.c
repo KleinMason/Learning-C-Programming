14f4fb4537c9c724c364e889193c3c82ed3f74fc
Mason Kleinsorge <mk@buildmidwestern.com>
2024-10-18 18:51:47 -0500

    Add necessary header files and new example functions

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <math.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 int circle()
 {
@@ -190,14 +192,134 @@ void bubbleSort()
 	printf("\nIterations: %d\n", iterations);
 }
 
+struct Player
+{
+	char name[12];
+	int score;
+};
+
 int structs()
 {
+	/*
+	struct = Collection of related members ("variables").
+					 They can be of different data types.
+					 Listed under one name in a block of memory.
+					 Very similar to classes in other languages, but no methods.
+	*/
+
+	struct Player player1;
+	struct Player player2;
+
+	strcpy(player1.name, "Link");
+	player1.score = 4;
+
+	strcpy(player2.name, "Zelda");
+	player2.score = 5;
+
+	printf("%s\n", player1.name);
+	printf("%d\n", player1.score);
+
+	printf("%s\n", player2.name);
+	printf("%d\n", player2.score);
+
+	return 0;
+}
+
+typedef struct
+{
+	char name[25];
+	char password[12];
+	int id;
+} User;
+
+int typeDef()
+{
+	// typedef = reserved keyword that gives an existing datatype a "nickname"
+
+	User user1 = {"Mason", "password123", 123456789};
+	User user2 = {"Klein", "greatPasswd", 987654321};
+
+	printf("%s\n", user1.name);
+	printf("%s\n", user1.password);
+	printf("%d\n", user1.id);
+
+	printf("%s\n", user2.name);
+	printf("%s\n", user2.password);
+	printf("%d\n", user2.id);
+
+	return 0;
+}
+
+struct Student
+{
+	char name[12];
+	float gpa;
+};
+
+void arrayOfStructs()
+{
+	struct Student student1 = {"Spongebob", 3.0};
+	struct Student student2 = {"Patrick", 2.5};
+	struct Student student3 = {"Sandy", 3.8};
+	struct Student student4 = {"Squidward", 2.9};
+
+	struct Student students[] = {student1, student2, student3, student4};
+	const int STUDENTS_SIZE = sizeof(students) / sizeof(students[0]);
+
+	for (int i = 0; i < STUDENTS_SIZE; i++)
+	{
+		printf("%-14s %.2f\n", students[i].name, students[i].gpa);
+	}
+}
+
+enum Day
+{
+	Sunday = 1,
+	Monday = 2,
+	Tuesday = 3,
+	Wednesday = 4,
+	Thursday = 5,
+	Friday = 6,
+	Saturday = 7
+};
+
+void enumExample()
+{
+	enum Day today = Saturday;
+
+	if (today == Sunday || today == Saturday)
+	{
+		printf("It's the weekend! Party time!\n");
+	}
+	else
+	{
+		printf("I have to work today.\n");
+	}
+}
+
+void pseudoRandomNumbers()
+{
+	/*
+	pseudo random numbers:
+			A set of values or elements that are statistically random.
+			(Don't use these for any sort of security)
+	*/
+
+	// srand = (s): seed, (rand): random
+	srand(time(0)); // current time as a seed
+
+	int number1 = rand() % 20 + 1; // random number between 1-6
+	int number2 = rand() % 20 + 1;
+	int number3 = rand() % 20 + 1;
+
+	printf("%d\n", number1);
+	printf("%d\n", number2);
+	printf("%d\n", number3);
 }
 
 int main()
 {
-
-	structs();
+	pseudoRandomNumbers();
 	return 0;
 }
 
